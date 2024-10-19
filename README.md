ec6278500de4f61c3e93f9f1200c865521196485
Fernando <asialer05@hotmail.com>
2024-10-18 21:04:51 -0500

    Proyecto base

---
+++ README.md
@@ -0,0 +1,2 @@
+# Servicio de productos de DFOOD
+> Este servicio se encargara de gestionar los productos y combos existentes. 
---
+++ src/main/java/com/fernando/ms/products/app/dfood_products_service/DfoodProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.products.app.dfood_products_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfoodProductsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfoodProductsServiceApplication.class, args);
+	}
+
+}
